0ce234fea079065d5bc1a2ab989c5ba530e80f8e
kushagradangi <kushagradangi10@gmail.com>
2026-07-22 17:06:18 +0530

    integrated tiny mce

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -9,6 +9,14 @@
   <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+
+  <script src="https://cdn.tiny.cloud/1/pnvzv73ewgkguulsu7vgpj2cfohqa7f65nwkhv8mfg5ol5fz/tinymce/8/tinymce.min.js" referrerpolicy="origin" crossorigin="anonymous"></script>
+  <script>
+    tinymce.init({
+        selector:'#mytextarea'
+    })
+  </script>
+  
 </head>
 <body>
 
@@ -56,6 +64,7 @@
                         <div class="form-group">
                             <label>Description</label>
                             <textarea
+                                id="mytextarea"
                                 class="form-control"
                                 rows="4"
                                 name="description">
